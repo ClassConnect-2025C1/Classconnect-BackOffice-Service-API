01076bce1a63113088cf177a302b5e47eb1b60a8
Matias Rea <matias32p@gmail.com>
2025-05-31 19:39:46 -0300

    feat: db client

---
+++ src/app/db/db_client.py
@@ -0,0 +1,16 @@
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from ..config.config import get_settings
+
+_settings = get_settings()
+_client: AsyncIOMotorClient | None = None
+
+
+def get_client() -> AsyncIOMotorClient:
+    global _client
+    if _client is None:
+        _client = AsyncIOMotorClient(_settings.mongo_uri)
+    return _client
+
+
+def get_db() -> AsyncIOMotorDatabase:
+    return get_client()[_settings.mongo_db]
