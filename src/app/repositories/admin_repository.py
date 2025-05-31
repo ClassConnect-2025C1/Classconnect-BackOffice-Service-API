c2e0867226e63257702745d70081e1e3b99a66ee
Matias Rea <matias32p@gmail.com>
2025-05-31 20:19:15 -0300

    feat: get admin by id in repository

--- src/app/repositories/admin_repository.py
+++ src/app/repositories/admin_repository.py
@@ -2,13 +2,15 @@ from pydantic import EmailStr
 from datetime import datetime, timezone
 from src.app.entities.admin_data import AdminDTA
 from pymongo.database import Database
+from pymongo.collection import Collection
+from bson import ObjectId
 
 REPOSITORY = "admin"
 
 
 class AdminRepository:
     def __init__(self, db: Database) -> None:
-        self.collection = db[REPOSITORY]
+        self.collection: Collection = db[REPOSITORY]
 
     def create(self, email: EmailStr, hashed_password: str, other_id: str) -> AdminDTA:
         admin_data = {
@@ -25,4 +27,22 @@ class AdminRepository:
             hashed_password=hashed_password,
             signup_date=admin_data["signup_date"],
             other_id=other_id,
+        )
+
+    def get_by_id(self, admin_id: str) -> AdminDTA | None:
+        try:
+            _id = ObjectId(admin_id)
+        except Exception:
+            return None
+
+        admin_data = self.collection.find_one({"_id": _id})
+        if not admin_data:
+            return None
+
+        return AdminDTA(
+            id=str(admin_data["_id"]),
+            email=admin_data["email"],
+            hashed_password=admin_data["hashed_password"],
+            signup_date=admin_data["signup_date"],
+            other_id=str(admin_data["other_id"]),
         )
