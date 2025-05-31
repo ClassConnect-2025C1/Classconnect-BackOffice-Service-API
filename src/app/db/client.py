deef5213be87f0024d820df160a10a0fa42824d0
Matias Rea <matias32p@gmail.com>
2025-05-31 13:36:32 -0300

    feat: connect mongo db

--- src/app/db/client.py
+++ src/app/db/client.py
@@ -1,4 +1,4 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from ..config.config import get_settings
 
 _settings = get_settings()
@@ -10,3 +10,6 @@ def get_client() -> AsyncIOMotorClient:
     if _client is None:
         _client = AsyncIOMotorClient(_settings.mongo_uri)
     return _client
+
+def get_db() -> AsyncIOMotorDatabase:
+    return get_client()[_settings.mongo_db]
