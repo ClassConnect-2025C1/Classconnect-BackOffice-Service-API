0c58d6f01561a23291b89cd38576acdf3472ef82
Matias Rea <matias32p@gmail.com>
2025-05-31 21:29:15 -0300

    feat: admin already exist exception

--- src/app/exceptions/exceptions.py
+++ src/app/exceptions/exceptions.py
@@ -2,3 +2,9 @@ class AdminNotFoundError(Exception):
     def __init__(self, creator_id: str):
         self.creator_id = creator_id
         super().__init__(f"Creator with id '{creator_id}' not found.")
+
+
+class AdminAlreadyExistsError(Exception):
+    def __init__(self, email: str):
+        self.email = email
+        super().__init__(f"Admin with email '{email}' already exists.")
