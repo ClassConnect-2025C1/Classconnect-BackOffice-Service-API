cf1f7f97ee38894cc3d9e746a9090bee343040ab
Matias Rea <matias32p@gmail.com>
2025-06-02 01:39:05 -0300

    feat Wrong password and cant get data from token

--- src/app/exceptions/exceptions.py
+++ src/app/exceptions/exceptions.py
@@ -1,10 +1,21 @@
 class AdminNotFoundError(Exception):
     def __init__(self, creator_id: str):
         self.creator_id = creator_id
-        super().__init__(f"Creator with id '{creator_id}' not found.")
+        super().__init__(f"Admin identified by '{creator_id}' not found.")
 
 
 class AdminAlreadyExistsError(Exception):
     def __init__(self, email: str):
         self.email = email
         super().__init__(f"Admin with email '{email}' already exists.")
+
+
+class WrongPasswordError(Exception):
+    def __init__(self, email: str):
+        self.email = email
+        super().__init__(f"Wrong password for admin with email '{email}'.")
+
+
+class GetDataFromTokenError(Exception):
+    def __init__(self):
+        super().__init__(f"Error getting data from token")
