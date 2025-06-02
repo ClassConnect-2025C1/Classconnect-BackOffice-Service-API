98cf6b3d18756b32becf5744aa33eb1971e6d02f
Matias Rea <matias32p@gmail.com>
2025-06-02 18:15:57 -0300

    feat: config host and port

--- src/app/config/config.py
+++ src/app/config/config.py
@@ -3,13 +3,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    mongo_uri: str = "mongodb://mongo:27017"
-    mongo_db: str = "classconnect_db"
+    mongo_uri: str = "mongodb://localhost:27017"
+    mongo_db: str = "backoffice_db"
     log_level: str = "INFO"
+    host: str = "127.0.0.1"
+    port: int = 8000
 
-    model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
+    model_config = SettingsConfigDict(case_sensitive=False)
 
 
 @lru_cache
 def get_settings() -> Settings:
     return Settings()
+
+
+if __name__ == "__main__":
+    print("✅ URI cargada:", get_settings().mongo_uri)
